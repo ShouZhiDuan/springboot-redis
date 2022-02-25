dd1523fe083574d72b4222c5594684390ef7fca4
段守志 <dszazhy1314@163.com>
2022-02-25 15:36:28 +0800

    fix code

--- src/test/java/com/redis/SpringbootRedisApplicationTests.java
+++ src/test/java/com/redis/SpringbootRedisApplicationTests.java
@@ -3,19 +3,29 @@ package com.redis;
 import com.redis.dto.UserDto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
 
 @Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootRedisApplicationTests {
 
-    @Resource
-    RedisTemplate<String, Object> redisTemplate;
+//    @Resource
+//    RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    RedisTemplate<Object, Object> redisTemplate;
+
+
+
 
     @Test
     void set() {
